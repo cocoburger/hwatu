66f3457604f99b0dc44441a12f6236b55b255597
burger <fsm15@naver.com>
2024-01-24 00:43:21 +0900

    refactor: 웹 컴포넌트로 리팩토링 진행중

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ fetch('../data/data.json').then((result) => result.json())
     .then((cardData) => {
       cards = [...cardData]
       shuffleCards()
-      generatedCards()
+      generateCards()
     })
 
 
 function shuffleCards(  ) {
   let currentIndex = cards.length,
       randomIndex,
-      temporaryValue;
+      temporaryValue
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex -= 1
@@ -27,20 +27,25 @@ function shuffleCards(  ) {
   }
 }
 
-function generatedCards() {
+function generateCards() {
   for ( let card of cards ) {
-    const cardElement = document.createElement('div')
-    cardElement.classList.add('card')
-    cardElement.setAttribute('data-name', card.name)
-    cardElement.innerHTML = `
+    const cardElement = generateCard(card)
+    gridContainer.appendChild(cardElement)
+    cardElement.addEventListener('click', flipCard)
+  }
+}
+
+function generateCard (card) {
+  const cardElement = document.createElement('div')
+  cardElement.classList.add('card')
+  cardElement.setAttribute('data-name', card.name)
+  cardElement.innerHTML = `
       <div class='front'>
         <img class='front-image' src=${card.image} alt='card'>
       </div>
       <div class='back'></div>
     `
-    gridContainer.appendChild(cardElement)
-    cardElement.addEventListener('click', flipCard)
-  }
+  return cardElement
 }
 
 function flipCard() {
@@ -96,5 +101,5 @@ function restart() {
   score = 0
   document.querySelector('.score').textContent = score
   gridContainer.innerHTML = ""
-  generatedCards()
+  generateCards()
 }
